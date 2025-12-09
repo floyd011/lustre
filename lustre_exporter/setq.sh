4c3f51faa74979346ca0b52ddbc7cb627feea4dd
Milorad Spasic <dbdeveloperexpert@gmail.com>
2025-12-09 07:42:03 +0100

    Fix formatting issues in setq.sh

--- lustre_exporter/setq.sh
+++ lustre_exporter/setq.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while read -r USERNAME; do
-             [[ -z "$USERNAME" ]] || [[ "$USERNAME" == "data" ]] && continue
+    [[ -z "$USERNAME" ]] || [[ "$USERNAME" == "data" ]] && continue
     
-             lfs setquota -u "$USERNAME" --block-softlimit 1024G --block-hardlimit 0 /lustre    
+    lfs setquota -u "$USERNAME" --block-softlimit 1024G --block-hardlimit 0 /lustre    
 done < <(lfs find /lustre/data -maxdepth 1 -type d | awk -F/ 'NF>3 && $0 != "/lustre/data" {print $NF}') 
